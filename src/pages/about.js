0f2c048ca6af7902d7be2e14792f49d370b54bf9
Dan Meuse <danny.meuse@gmail.com>
2021-08-20 20:08:10 -0700

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const AboutMe = () => {
+  return (
+    <Layout pageTitle='About Me'>    
+      <p>Hello there! My name is Danny. I love making websites and learning more about the internet!</p>
+    </Layout>
+  )
+}
+
+export default AboutMe
