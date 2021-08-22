c05181a3e70d7ed50f3d11f06d4fc400e92cd80f
Dan Meuse <danny.meuse@gmail.com>
2021-08-21 21:26:53 -0700

    Finished Gatsby Tutorial Part 5

---
+++ src/pages/{mdx.slug}.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const BlogPost = () => {
+  return (
+    <Layout pageTitle="Super Cool Blog Posts">
+      <p>My posts will eventually go here</p>
+    </Layout>
+  )
+}
+
+
+export default BlogPost
